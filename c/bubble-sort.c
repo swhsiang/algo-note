257bef295f331c7283406d103774567bf65e6688
swhsiang <swh@hsiang.io>
2017-10-02 09:35:47 +0800

    Improve bubble-sort.c Make it more efficient

--- c/bubble-sort.c
+++ c/bubble-sort.c
@@ -17,13 +17,17 @@ void swap(int *a, int *b) {
 //   the value of len would not fit our expectation
 // }
 void sort(int array[], size_t len) {
-    int i, j;
-    printf("len : %zu", len);
+    int i, j, swaped;
     for (i = 0; i < len - 1; i ++ ) {
+        swaped = 0;
         for (j = 0; j < len - i - 1; j ++) {
             if (array[j] > array[j + 1]) {
+                swaped = 1;
                 swap(&array[j], &array[j+1]);
             }
+        }
+        if (!swaped) {
+          break;
         }
     }
 }
@@ -32,17 +36,16 @@ void printArr(int array[], size_t len) {
     int i = 0;
 
     for (i = 0; i < len; i ++) {
-        printf("%d ", array[i]);
+        printf("%4d ", array[i]);
     }
+    printf("\n");
 }
 
 int main(void) {
     // 7
     int testArr[] = {2, 61, 23, 231, 51, 56, 66};
     printArr(testArr, sizeof(testArr) / sizeof(int));
-    printf("\n");
     sort(testArr, sizeof(testArr) / sizeof(int));
-    printf("\n");
     printArr(testArr, sizeof(testArr) / sizeof(int));
     return 0;
 }
